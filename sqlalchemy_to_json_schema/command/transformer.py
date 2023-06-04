f330a0b41fca554c86fae9d1fad3ff3119f41bd3
Daniele Esposti <daniele@withplum.com>
2023-06-04 18:18:04 +0100

    Removed DictWalker

--- sqlalchemy_to_json_schema/command/transformer.py
+++ sqlalchemy_to_json_schema/command/transformer.py
@@ -1,90 +1,8 @@
 import inspect
 from abc import ABC, abstractmethod
-from collections import deque
 from types import ModuleType
 
 from sqlalchemy_to_json_schema import Schema, SchemaFactory
-
-
-def apply(q, v, *args):
-    if callable(q):
-        return q(v, *args)
-    else:
-        return q == v
-
-
-class ContainerHandler:
-    def identity(self, *args):
-        return args
-
-    def __call__(self, walker, ctx, d, k):
-        return ctx(walker, self.identity, d)
-
-
-class PathContext:
-    def __init__(self):
-        self.path = []
-
-    def push(self, v):
-        self.path.append(v)
-
-    def pop(self):
-        self.path.pop()
-
-    def __call__(self, walker, fn, value):
-        return fn(self.path, value)
-
-
-class DictWalker:
-    context_factory = PathContext
-    handler_factory = ContainerHandler
-
-    def __init__(self, qs, handler=None, context_factory=None):
-        self.qs = qs
-        self.context_factory = context_factory or self.__class__.context_factory
-        self.handler = handler or self.__class__.handler_factory()
-
-    def on_found(self, ctx, d, k):
-        yield self.handler(self, ctx, d, k)
-
-    def create_context(self, ctx=None):
-        return ctx or self.context_factory()
-
-    def walk(self, d, qs=None, depth=-1, ctx=None):
-        qs = qs or self.qs
-        ctx = self.create_context(ctx)
-        return self._walk(ctx, deque(self.qs), d, depth=depth)
-
-    def _walk(self, ctx, qs, d, depth):
-        if depth == 0:
-            return
-
-        if not qs:
-            return
-
-        if hasattr(d, "keys"):
-            for k, v in list(d.items()):
-                ctx.push(k)
-                if apply(qs[0], k, v):
-                    q = qs.popleft()
-                    yield from self._walk(ctx, qs, d[k], depth - 1)
-                    if len(qs) == 0:
-                        yield from self.on_found(ctx, d, k)
-                    qs.appendleft(q)
-                else:
-                    yield from self._walk(ctx, qs, d[k], depth)
-                ctx.pop()
-            return
-        elif isinstance(d, (list, tuple)):
-            for i, e in enumerate(d):
-                ctx.push(i)
-                yield from self._walk(ctx, qs, e, depth)
-                ctx.pop()
-            return
-        else:
-            return
-
-    iterate = walk  # for backward compatibility
 
 
 class Transformer(ABC):
@@ -138,10 +56,22 @@ class OpenAPI3Transformer(Transformer):
 
         self.oas2transformer = OpenAPI2Transformer(schema_factory)
 
+    def replace_ref(self, d, old_prefix, new_prefix):
+        if isinstance(d, dict):
+            for k, v in d.items():
+                if k == "$ref":
+                    d[k] = v.replace(old_prefix, new_prefix)
+                else:
+                    self.replace_ref(v, old_prefix, new_prefix)
+        elif isinstance(d, list):
+            for item in d:
+                self.replace_ref(item, old_prefix, new_prefix)
+
     def transform(self, rawtarget: ModuleType, depth: int) -> Schema:
         d = self.oas2transformer.transform(rawtarget, depth)
-        for _, sd in DictWalker(["$ref"]).walk(d):
-            sd["$ref"] = sd["$ref"].replace("#/definitions/", "#/components/schemas/")
+
+        self.replace_ref(d, "#/definitions/", "#/components/schemas/")
+
         if "components" not in d:
             d["components"] = {}
         if "schemas" not in d["components"]:
